08b6c0add968b6a88193ee39883d7fab7865b5d8
Alane Rocha <161375811+alanerocha@users.noreply.github.com>
2025-08-25 10:15:50 -0300

    Atualizações do codigo

---
+++ Fiap.Versionador/Util.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Introducion
+{
+    public class Util
+    {
+        public enum DiasSemana
+        {
+            Segunda,
+            Terca,
+            Quarta,
+            Quinta,
+            Sexta,
+            Sabado,
+            Domingo
+        }
+
+        public enum TiposErro
+        {
+            ErroLogin,
+            ErrodeConta,
+            ErroExpection,
+            ErroFatal
+        }
+
+        public void Log(TiposErro tipoErro)
+        {
+            Console.WriteLine(String.Concat("Erro no dia", System.DateTime.Now, tipoErro));
+        }
+
+        public string RetornarEnderecoPor(string CEP)
+        {
+            return "TBD";
+        }
+    }
+}
